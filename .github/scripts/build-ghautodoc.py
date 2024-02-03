d69d05762b43e900dae38a4b25b068e39eceabe9
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-02-02 22:14:15 -0800

    WIP

--- .github/scripts/build-ghautodoc.py
+++ .github/scripts/build-ghautodoc.py
@@ -8,6 +8,7 @@ print("ghautodoc")
 github_context = json.loads(os.environ["GITHUB_CONTEXT"])
 
 pr_url = github_context["event"]["pull_request"]["url"]
+pr_head_sha = github_context["event"]["pull_request"]["head"]["sha"]
 # diff_url = github_context["event"]["pull_request"]["diff_url"]
 
 headers = {
@@ -32,4 +33,39 @@ def get_files(pr_url: str, headers: dict) -> list:
     return res.json()
 
 
-print(get_files(pr_url, headers))
+def add_comment(
+    pr_url: str,
+    headers: dict,
+    body: str,
+    commit_id: str,
+    path: str,
+    line: int,
+):
+    res = requests.post(
+        f"{pr_url}/comments",
+        headers=headers,
+        json=dict(
+            body=body,
+            commit_id=commit_id,
+            path=path,
+            line=line,
+        ),
+        timeout=30,
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+# https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/comments \
+#  -d '{"body":"Great stuff!","commit_id":"6dcb09b5b57875f334f61aebed695e2e4193db5e","path":"file1.txt","start_line":1,"start_side":"RIGHT","line":2,"side":"RIGHT"}'
+
+for file_change in get_files(pr_url, headers):
+    # TODO: extract lines from patch
+    add_comment(
+        pr_url=pr_url,
+        headers=headers,
+        body="test",
+        commit_id=pr_head_sha,
+        path=file_change["filename"],
+        line=1,
+    )
