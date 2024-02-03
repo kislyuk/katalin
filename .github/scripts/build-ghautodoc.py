7c979c80ecf946a346937e6a3332081ddc6fd81f
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-02-02 16:52:38 -0800

    WIP

--- .github/scripts/build-ghautodoc.py
+++ .github/scripts/build-ghautodoc.py
@@ -8,14 +8,16 @@ print("ghautodoc")
 github_context = json.loads(os.environ["GITHUB_CONTEXT"])
 
 pr_url = github_context["event"]["pull_request"]["url"]
-diff_url = github_context["event"]["pull_request"]["diff_url"]
+# diff_url = github_context["event"]["pull_request"]["diff_url"]
 
 headers = {
     "Accept": "application/vnd.github+json",
     "Authorization": f"Bearer {github_context['token']}",
 }
 
-res = requests.get(diff_url, headers=headers)
+res = requests.get(
+    pr_url, headers=dict(headers, Accept="application/vnd.github.v3.diff"), timeout=30
+)
 res.raise_for_status()
 
 print(res.content)
