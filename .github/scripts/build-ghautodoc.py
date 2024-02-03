bcce841e693f6a7e49eb5ad6dc007dc7f91a4826
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-02-02 16:49:14 -0800

    WIP

--- .github/scripts/build-ghautodoc.py
+++ .github/scripts/build-ghautodoc.py
@@ -1,7 +1,21 @@
-from fastcore.all import *
-from ghapi import *
+import json
+import os
+
+import requests
 
 print("ghautodoc")
 
-import os
-print(os.environ)
+github_context = json.loads(os.environ["GITHUB_CONTEXT"])
+
+pr_url = github_context["event"]["pull_request"]["url"]
+diff_url = github_context["event"]["pull_request"]["diff_url"]
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {github_context['token']}",
+}
+
+res = requests.get(diff_url, headers=headers)
+res.raise_for_status()
+
+print(res.content)
